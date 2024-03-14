518339c7bcaa8863d15dbe0577ce8024d6a372a8
EC2 Default User <ec2-user@ip-172-31-11-80.ap-northeast-1.compute.internal>
2024-03-14 06:54:35 +0000

    投稿機能完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,24 @@
 class BooksController < ApplicationController
   def index
-    
+    @books = Book.all
+    @book = Book.new
   end
   
   def create
-    
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render_to root_path
+    end
   end
   
   def show
-    
   end
+  
+  private
+  
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  resources :book, only: [:index, :create, :show, :edit]
+  # get '/books' => 'books#index'
+  resources :books, only: [:index, :create, :show, :edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
